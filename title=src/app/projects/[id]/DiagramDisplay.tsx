1293502ec699d5014d08e532237e7a9d52aec8d6
seyitumturk <seyitumturk@gmail.com>
2025-02-18 01:21:26 -0400

    nine

--- title=src/app/projects/[id]/DiagramDisplay.tsx
+++ title=src/app/projects/[id]/DiagramDisplay.tsx
@@ -1,21 +1,21 @@
   <div>
     <div 
       className="md:hidden fixed left-0 right-0 z-50" 
-      style={{ bottom: showPromptPanel ? "50vh" : "0" }}
+      style={{ bottom: props.showPromptPanel ? "50vh" : "0" }}
     >
       <DiagramControls 
-        showPromptPanel={showPromptPanel}
-        setShowPromptPanel={setShowPromptPanel}
-        scale={scale}
-        setScale={setScale}
-        setPosition={setPosition}
-        downloadSVG={downloadSVG}
-        downloadPNG={downloadPNG}
-        showExportMenu={showExportMenu}
-        setShowExportMenu={setShowExportMenu}
+        showPromptPanel={props.showPromptPanel}
+        setShowPromptPanel={props.setShowPromptPanel}
+        scale={props.scale}
+        setScale={props.setScale}
+        setPosition={props.setPosition}
+        downloadSVG={props.downloadSVG}
+        downloadPNG={props.downloadPNG}
+        showExportMenu={props.showExportMenu}
+        setShowExportMenu={props.setShowExportMenu}
       />
     </div>
 
-    <div className={`relative flex-1 flex flex-col transition-all duration-300 ease-in-out ${showPromptPanel ? "md:ml-96" : "md:ml-0"}`}>
+    <div className={`relative flex-1 flex flex-col transition-all duration-300 ease-in-out ${props.showPromptPanel ? "md:ml-96" : "md:ml-0"}`}>
     </div>
   </div>
